4183d7790a790f16daac2ba158217fc76fb8f1ab
Srinithi A <srinithiashokan30@gmail.com>
2025-12-18 08:10:51 +0530

    Update user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,6 +5,6 @@ import "time"
 type User struct {
 	ID   int       `json:"id"`
 	Name string    `json:"name"`
-	Dob  time.Time `json:"dob"` // ✅ time.Time
+	Dob  time.Time `json:"dob"` 
 	Age  int       `json:"age,omitempty"`
 }
